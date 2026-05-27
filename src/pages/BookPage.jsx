320b58bb6adefa47c311218718a5b42da0fed3d6
AlBalboni <balbonialejandro@gmail.com>
2026-05-27 15:48:10 +0200

    shows the number of books per genre

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,41 +1,97 @@
 import BookList from "../components/BookList";
 import { useState, useEffect } from "react";
-{/*toasts*/ }
-import { toast } from "react-toastify";
 import { useSearchParams } from "react-router-dom";
 import { FaBars, FaSearch } from "react-icons/fa";
+{/*toasts*/ }
+import { toast } from "react-toastify";
 
 const BookPage = () => {
+  {/*for sorting by alphabetical order and page Count*/ }
+  const [sortOption, setSortOption] = useState("");
+  const [showOption, setShowOption] = useState(false);
   {/*for searching bar*/ }
   const [search, setSearch] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   {/*for sorting genre filter*/ }
   const [sortGenre, setSortGenre] = useState("");
-  {/*for sorting by alphabetical order and page Count*/ }
-  const [sortOption, setSortOption] = useState("");
-  const [showOption, setShowOption] = useState(false);
+  const [genreBooks, setGenreBooks] = useState([]);
 
   useEffect(() => {
-  const message = searchParams.get("toast");
-  const type = searchParams.get("type");
 
-  if (message) {
-    if (type === "success") {
-      toast.success(message);
-    } else if (type === "error") {
-      toast.error(message);
-    } else if (type === "info") {
-      toast.info(message);
-    } else if (type === "warning") {
-      toast.warning(message);
-    }
+    const fetchBooks = async () => {
+      const BASE_URL = import.meta.env.VITE_API_URL;
+
+      const response = await fetch(`${BASE_URL}/books`);
+      const data = await response.json();
+      setGenreBooks(data);
+    };
+    fetchBooks();
+
+    const message = searchParams.get("toast");
+    const type = searchParams.get("type");
+
+    if (message) {
+      if (type === "success") {
+        toast.success(message);
+      } else if (type === "error") {
+        toast.error(message);
+      } else if (type === "info") {
+        toast.info(message);
+      } else if (type === "warning") {
+        toast.warning(message);
+      }
 
     setSearchParams({});
   }
- }, []);
+}, []);
+
+{/*Counts the number of books for each genre and displays the count to the right*/ }
+ const genreCounts = {
+    Adventure: 0,
+    Biography: 0,
+    Classic: 0,
+    Comedy: 0,
+    Crime: 0,
+    Dystopian: 0,
+    Epic: 0,
+    Essay: 0,
+    Fantasy: 0,
+    Fiction: 0,
+    "Graphic Novel": 0,
+    Historical: 0,
+    "Historical Fiction": 0,
+    Horror: 0,
+    Mystery: 0,
+    Philosophical: 0,
+    Poetry: 0,
+    Psychological: 0,
+    Religious: 0,
+    Realism: 0,
+    Romance: 0,
+    Satire: 0,
+    "Science Fiction": 0,
+    "Social Criticism": 0,
+    Theater: 0,
+    Thriller: 0,
+  };
+
+  genreBooks.forEach((book) => {
+    if (Array.isArray(book.genre)) {
+
+      book.genre.forEach((g) => {
+        if (genreCounts[g] !== undefined) {
+          genreCounts[g]++;
+        }
+      });
+
+    } else {
+      if (genreCounts[book.genre] !== undefined) {
+        genreCounts[book.genre]++;
+      }
+    }
+  });
 
   return (
-
     <>
 <div className="flex items-center justify-between gap-4 px-6 py-6 bg-white flex-wrap">
 
@@ -45,7 +101,7 @@ const BookPage = () => {
       onClick={() => setShowOption(!showOption)}
       className="border rounded-lg p-3 hover:bg-gray-100"
     >
-      <FaBars />
+      <FaBars/>
     </button>
 
     {showOption && (
@@ -102,40 +158,40 @@ const BookPage = () => {
     className="border rounded-lg py-2 px-4"
   >
     <option value="">All genres</option>
-    <option value="Adventure">Adventure</option>
-    <option value="Biography">Biography</option>
-    <option value="Classic">Classic</option>
-    <option value="Comedy">Comedy</option>
-    <option value="Crime">Crime</option>
-    <option value="Dystopian">Dystopian</option>
-    <option value="Epic">Epic</option>
-    <option value="Essay">Essay</option>
-    <option value="Fantasy">Fantasy</option>
-    <option value="Fiction">Fiction</option>
-    <option value="Graphic Novel">Graphic Novel</option>
-    <option value="Historical">Historical</option>
-    <option value="Historical Fiction">Historical Fiction</option>
-    <option value="Horror">Horror</option>
-    <option value="Mystery">Mystery</option>
-    <option value="Philosophical">Philosophical</option>
-    <option value="Poetry">Poetry</option>
-    <option value="Psychological">Psychological</option>
-    <option value="Religious">Religious</option>
-    <option value="Realism">Realism</option>
-    <option value="Romance">Romance</option>
-    <option value="Satire">Satire</option>
-    <option value="Social Criticism">Social Criticism</option>
-    <option value="Science Fiction">Science Fiction</option>
-    <option value="Theater">Theater</option>
-    <option value="Thriller">Thriller</option>
+    <option value="Adventure">Adventure ({genreCounts.Adventure})</option>
+    <option value="Biography">Biography ({genreCounts.Biography})</option>
+    <option value="Classic">Classic ({genreCounts.Classic})</option>
+    <option value="Comedy">Comedy ({genreCounts.Comedy})</option>
+    <option value="Crime">Crime ({genreCounts.Crime})</option>
+    <option value="Dystopian">Dystopian ({genreCounts.Dystopian})</option>
+    <option value="Epic">Epic ({genreCounts.Epic})</option>
+    <option value="Essay">Essay ({genreCounts.Essay})</option>
+    <option value="Fantasy">Fantasy ({genreCounts.Fantasy})</option>
+    <option value="Fiction">Fiction ({genreCounts.Fiction})</option>
+    <option value="Graphic Novel">Graphic Novel ({genreCounts["Graphic Novel"]})</option>
+    <option value="Historical">Historical ({genreCounts.Historical})</option>
+    <option value="Historical Fiction">Historical Fiction ({genreCounts["Historical Fiction"]})</option>
+    <option value="Horror">Horror ({genreCounts.Horror})</option>
+    <option value="Mystery">Mystery ({genreCounts.Mystery})</option>
+    <option value="Philosophical">Philosophical ({genreCounts.Philosophical})</option>
+    <option value="Poetry">Poetry ({genreCounts.Poetry})</option>
+    <option value="Psychological">Psychological ({genreCounts.Psychological})</option>
+    <option value="Religious">Religious ({genreCounts.Religious})</option>
+    <option value="Realism">Realism ({genreCounts.Realism})</option>
+    <option value="Romance">Romance ({genreCounts.Romance})</option>
+    <option value="Satire">Satire ({genreCounts.Satire})</option>
+    <option value="Social Criticism">Social Criticism ({genreCounts["Social Criticism"]})</option>
+    <option value="Science Fiction">Science Fiction ({genreCounts["Science Fiction"]})</option>
+    <option value="Theater">Theater ({genreCounts.Theater})</option>
+    <option value="Thriller">Thriller ({genreCounts.Thriller})</option>
   </select>
 
-</div>
-  <BookList 
-    search={search}
-    sortGenre={sortGenre}
-    sortOption={sortOption}
-  />
+  </div>
+    <BookList 
+      search={search}
+      sortGenre={sortGenre}
+      sortOption={sortOption}
+    />
   </>
   );
 };
