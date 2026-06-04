7fb386dc6d963992f76967e370ec718344b0762a
AlBalboni <balbonialejandro@gmail.com>
2026-06-04 13:27:30 +0200

    hopefully last bug fix for reviews

--- src/loaders/bookLoader.js
+++ src/loaders/bookLoader.js
@@ -1,16 +1,15 @@
 const bookLoader = async ({ params }) => {
   const BASE_URL = import.meta.env.VITE_API_URL;
-  const REVIEW_URL = import.meta.env.VITE_REVIEW_API_URL;
+{/*fixed bug with netlify deployment doesnt allow ?bookId*/}
+  const response = await fetch(
+    `${BASE_URL}/books/${params.id}`
+  );
 
-  const reviewResponse = await fetch(`${REVIEW_URL}?bookId=${params.id}`);
-  const response = await fetch(`${BASE_URL}/books/${params.id}`);
-  if (!response.ok) throw new Error("Book info not found");
-  if (!reviewResponse.ok) throw new Error("Reviews not found");
-  const [bookData, reviewsData] = await Promise.all([
-    response.json(),
-    reviewResponse.json()
-  ]);
-  return { ...bookData, reviews: reviewsData };
+  if (!response.ok) {
+    throw new Error("Book info not found");
+  }
+
+  return await response.json();
 };
 
 export default bookLoader;
